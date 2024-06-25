ab9e1b9bc2bd990b5f083122223f78759cef3c12
nonnt66v2 <lorenzo.dipalo001@studenti.uniparthenope.it>
2024-06-25 02:05:31 +0200

    Inserito l'indirizzo IP pubblico nel file my-ip.txt

---
+++ autoPush.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Esegui il comando per ottenere l'indirizzo IP pubblico
+IP_PUBBLICO=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
+# Creare un file di testo chiamato "my-ip.txt"
+echo -n "Mio indirizzo IP pubblico: $IP_PUBBLICO" > my-ip.txt
+
+# Aggiungere il file al repository Git
+git add my-ip.txt
+
+# Committare i cambiamenti con un messaggio descrittivo
+git commit -m "Inserito l'indirizzo IP pubblico nel file my-ip.txt"
+
+# Pushare il mio repository su GitHub
+git push -u origin main
+
